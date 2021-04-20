2374880ecfead9d61e736b7f06318885624ea6da
Elinvynia <59487684+Elinvynia@users.noreply.github.com>
2021-04-20 15:43:26 +0200

    Add rustfmt.toml

--- src/lib.rs
+++ src/lib.rs
@@ -43,36 +43,21 @@ pub fn parse<P: AsRef<Path>>(path: P) -> Result<Vec<Entry>, DaylioError> {
 }
 
 /// Parse a `.csv` file into Daylio diary entries using your custom [`MoodConfig`].
-pub fn parse_with_config<P: AsRef<Path>>(
-    path: P,
-    config: MoodConfig,
-) -> Result<Vec<Entry>, DaylioError> {
-    let reader = ReaderBuilder::new()
-        .delimiter(b',')
-        .quote(b'"')
-        .from_path(path)?;
+pub fn parse_with_config<P: AsRef<Path>>(path: P, config: MoodConfig) -> Result<Vec<Entry>, DaylioError> {
+    let reader = ReaderBuilder::new().delimiter(b',').quote(b'"').from_path(path)?;
     let mut entries = vec![];
 
     for row in reader.into_deserialize() {
         let row: Row = row?;
 
         let datetime;
-        let test = NaiveDateTime::parse_from_str(
-            &format!("{} {:0>8}", row.full_date, row.time),
-            "%F %I:%M %P",
-        );
+        let test = NaiveDateTime::parse_from_str(&format!("{} {:0>8}", row.full_date, row.time), "%F %I:%M %P");
         if test.is_err() {
-            datetime = NaiveDateTime::parse_from_str(
-                &format!("{} {}", row.full_date, row.time),
-                "%F %H:%M",
-            )?;
+            datetime = NaiveDateTime::parse_from_str(&format!("{} {}", row.full_date, row.time), "%F %H:%M")?;
         } else {
             datetime = test?;
         }
-        let mood = config
-            .get(&row.mood)
-            .ok_or(DaylioError::MoodNotFound)?
-            .clone();
+        let mood = config.get(&row.mood).ok_or(DaylioError::MoodNotFound)?.clone();
         let activities = row.activities.split('|').map(|x| x.to_string()).collect();
         let notes = row.note;
 
